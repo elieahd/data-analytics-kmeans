8ad2e78ca58d1db5acde4712af90fe392c18e750
Elie Daher <elieahd@hotmail.com>
2018-04-22 21:30:33 +0200

    updating generator.py code

--- generator.py
+++ generator.py
@@ -2,11 +2,21 @@
 # imports
 import sys
 import random
+import numpy
 
 from pyspark import SparkContext
 from pyspark.mllib.random import RandomRDDs
 
+# constants
+MIN_MEAN_VALUE = 0
+MAX_MEAN_VALUE = 100
+STEPS = 0.1
+
 # methods
+def calculate_value_point(point):
+    values = point[0]
+    cluster = int(point[1])
+    return point
 
 # main code
 if len(sys.argv) != 6:
@@ -21,17 +31,24 @@ if len(sys.argv) != 6:
 
 # inputs
 file_name = sys.argv[1] + '.csv' # file name to be generated
-points = int(sys.argv[2]) # number of points to be generated --> rows
-clusters = int(sys.argv[3]) # number of clusters
-dimension = int(sys.argv[4]) # dimension of the data --> colones
+points = int(sys.argv[2]) # number of points to be generated
+count_cluster = int(sys.argv[3]) # number of clusters
+dimension = int(sys.argv[4]) # dimension of the data
 std = int(sys.argv[5]) # standard deviation
 
-# constants
-MIN_VALUE = 0
-MAX_VALUE = 100
-MU = 50
+sc = SparkContext("local", "generator") # spark context
 
-sc = SparkContext("local", "generator")
-normalValuesRDD = RandomRDDs.normalVectorRDD(sc, numRows = points, numCols = dimension)
-(MAX_VALUE + 1 - MIN_VALUE) + MIN_VALUE
-normalValuesRDD.map(lambda value : std * value)
+# array of the clusters : clusters = [0, 1, 2]
+clusters = sc.parallelize(range(0, count_cluster))
+
+# random means of each cluster : means_cluster = [ (0, [0.6, 80.9]), (1, [57.8, 20.2]), (2, [15.6, 49.9]) ]
+means_clusters = clusters.map(lambda cluster : (cluster, random.sample(numpy.arange(MIN_MEAN_VALUE, MAX_MEAN_VALUE, STEPS), dimension)))
+
+# creating random vector using normalVectorRDD
+random_values_vector = RandomRDDs.normalVectorRDD(sc, numRows = points, numCols = dimension, numPartitions = count_cluster, seed = 1L)
+
+# assiging a random cluster for each point
+cluster_normal_vector = random_values_vector.map(lambda point : (point.tolist(), random.randint(0, count_cluster - 1)))
+
+# re-calculating each value of each point based on the mean of a cluster
+data = cluster_normal_vector.map(lambda point : calculate_value_point(point))
