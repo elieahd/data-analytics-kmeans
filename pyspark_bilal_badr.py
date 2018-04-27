88090942a9cf53095ba488e2acec28094e7750d7
Bilal El Chami <bilalchami@live.com>
2018-04-27 21:35:41 +0200

    Generic split

--- pyspark_bilal_badr.py
+++ pyspark_bilal_badr.py
@@ -2,16 +2,18 @@ from pyspark.mllib.random import RandomRDDs
 import numpy
 import datetime
 
-# path = "data/iris_small.dat"
-path = "data/iris_clustering.dat"
+# path = "data/iris_clustering.dat"
+path = "data/iris_small.dat"
 numClusters = 4
 
 # methods
 def customSplit(row):
     values = row[0]
     index = row[1]
-    sepalLength, sepalWidth, petalLength, petalWidth, cluster = values.split(',')
-    return (index, [float(sepalLength), float(sepalWidth), float(petalLength), float(petalWidth), cluster])
+    dataItems = values.split(',')
+    for i in range(len(dataItems) - 1):
+        dataItems[i] = float(dataItems[i])
+    return (index, dataItems)
 
 def loadData(path):
     dataFromText = sc.textFile(path)
@@ -21,7 +23,7 @@ def loadData(path):
 #Random is not efficient!
 def initCentroids(data, numClusters):
     sample = sc.parallelize(data.takeSample(False, numClusters))
-    centroids = sample.map(lambda point : point[1][:-1]) 
+    centroids = sample.map(lambda point : point[1][:-1])
     return centroids.zipWithIndex().map(lambda point : (point[1], point[0]))
 
 def calculateDistance(centroid, dataPoint):
